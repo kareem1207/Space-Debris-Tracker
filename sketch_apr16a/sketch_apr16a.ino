184f39282f1b11e767093b40518943b132a40090
kareem1207 <kareemshaik1207@gmail.com>
2025-04-22 10:28:18 +0530

    Refactor servo control code: improve structure and add comments for clarity

--- sketch_apr16a/sketch_apr16a.ino
+++ sketch_apr16a/sketch_apr16a.ino
@@ -1,20 +1,28 @@
-
 #include <SoftwareSerial.h>
 #include <TinyGPS++.h>
+#include <Servo.h>
 
-SoftwareSerial gpsSerial(4, 3);
+SoftwareSerial gpsSerial(4, 3); // RX, TX for GPS
 TinyGPSPlus gps;
+
+Servo servoAzimuth;
+Servo servoAltitude;
+
+String inputString = "";
 
 void setup()
 {
   Serial.begin(9600);
   gpsSerial.begin(9600);
   Serial.println("Waiting for GPS signal...");
+
+  servoAzimuth.attach(9);   // Attach azimuth servo to pin 9
+  servoAltitude.attach(10); // Attach altitude servo to pin 10
 }
 
 void loop()
 {
-
+  // GPS Processing
   while (gpsSerial.available())
   {
     gps.encode(gpsSerial.read());
@@ -25,6 +33,43 @@ void loop()
       Serial.print(gps.location.lat(), 6);
       Serial.print(" | Longitude: ");
       Serial.println(gps.location.lng(), 6);
+    }
+  }
+
+  // Servo Control via Serial
+  while (Serial.available())
+  {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n')
+    {
+      processCommand(inputString);
+      inputString = "";
+    }
+    else
+    {
+      inputString += inChar;
+    }
+  }
+}
+
+void processCommand(String cmd)
+{
+  // Expected format: SERVO,azimuth,altitude
+  if (cmd.startsWith("SERVO"))
+  {
+    int firstComma = cmd.indexOf(',');
+    int secondComma = cmd.indexOf(',', firstComma + 1);
+
+    if (firstComma > 0 && secondComma > firstComma)
+    {
+      int az = cmd.substring(firstComma + 1, secondComma).toInt();
+      int alt = cmd.substring(secondComma + 1).toInt();
+
+      az = constrain(az, 0, 180);
+      alt = constrain(alt, 0, 180);
+
+      servoAzimuth.write(az);
+      servoAltitude.write(alt);
     }
   }
 }
